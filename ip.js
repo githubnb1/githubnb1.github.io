a7d00f2680e466172939d114fd30cff150e8da81
githubnb1 <71148121+githubnb1@users.noreply.github.com>
2022-01-22 13:25:39 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,3 +1,1 @@
-<script src="ip.js"></script>
-点击提示实例1: IP为 <a href="javascript:alertIp('171.118.23.19');">171.118.23.19</a>
-点击显示实例1: IP为 <a href="javascript:labelIp('showIpInfo','171.118.23.19');">171.118.23.19</a> 位置信息:<span id="showIpInfo" style="color:#FF0000"></span>
+1
